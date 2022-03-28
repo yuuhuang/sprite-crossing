ea32b2eeea868eb1f50b68d3f82f8adf65552c61
Neil <948459029@qq.com>
2022-03-28 23:47:47 +0800

    chores: breakpoint

--- src/components/Dialog/WorkDialog.vue
+++ src/components/Dialog/WorkDialog.vue
@@ -3,10 +3,10 @@
     <v-card>
       <v-card-title>
         <v-row>
-          <v-col xl="8" lg="8" md="8" cols="12">
+          <v-col :cols="$vuetify.breakpoint.mdAndUp ? 8 : 12">
             {{ workData.title }}
           </v-col>
-          <v-col v-if="!smallScreen" xl="4" lg="4" md="4" cols="12"
+          <v-col v-if="!$vuetify.breakpoint.smAndDown" cols="4"
             style="display: flex;align-items: center;justify-content: right">
             <nickname-avatar
               :nickname="workData.nickname"
@@ -18,26 +18,26 @@
       </v-card-title>
       <v-card-text class="hide-scroll">
         <v-row>
-          <v-col xl="9" lg="9" md="9" sm="12" ref="img-col">
+          <v-col :cols="$vuetify.breakpoint.mdAndUp ? 9 : 12" ref="img-col">
             <v-card
               class="img-card flex-center pointer-cursor"
               max-width="100%"
               min-width="100%"
-              :max-height="smallScreen ? '100%' : 480"
-              :min-height="smallScreen ? '100%' : 480"
+              :max-height="$vuetify.breakpoint.smAndDown ? '100%' : 480"
+              :min-height="$vuetify.breakpoint.smAndDown ? '100%' : 480"
               tile
               flat>
               <v-img
                 :src="workData.image"
                 max-width="100%"
-                :max-height="smallScreen ? '100%' : 480"
+                :max-height="$vuetify.breakpoint.smAndDown ? '100%' : 480"
                 contain
               >
                 <template v-slot:placeholder>
                   <v-skeleton-loader type="image" height="100%" tile></v-skeleton-loader>
                 </template>
               </v-img>
-              <div class="icon-options flex-center" v-if="!smallScreen && showOverlay">
+              <div class="icon-options flex-center" v-if="!$vuetify.breakpoint.smAndDown && showOverlay">
                 <eye class="gray-filter mr-2"></eye>
                 <span class="gray-filter mr-2">{{ workData.viewNum }}</span>
                 <comment class="gray-filter mr-2"></comment>
@@ -47,12 +47,12 @@
                   @click="clickHeart"></heart>
                 <span class="gray-filter mr-2">{{ workData.likeNum }}</span>
               </div>
-              <div class="overlay" v-if="!smallScreen && showOverlay"></div>
+              <div class="overlay" v-if="!$vuetify.breakpoint.smAndDown && showOverlay"></div>
             </v-card>
           </v-col>
           <v-col xl="3" lg="3" md="3" sm="12" class="pl-0 pr-0">
             <v-card flat>
-              <v-card-text v-if="smallScreen" class="pt-0 pb-0">
+              <v-card-text v-if="$vuetify.breakpoint.smAndDown" class="pt-0 pb-0">
                 <v-row>
                   <v-col class="pt-0" cols="5">
                     <nickname-avatar
@@ -123,19 +123,14 @@ export default {
       this.showOverlay = false;
     },
   },
-  computed: {
-    smallScreen() {
-      return (/sm|xs/).test(this.$vuetify.breakpoint.name);
-    }
-  },
   mounted() {
-    if (!this.smallScreen) {
+    if (!this.$vuetify.breakpoint.smAndDown) {
       this.$refs['img-col'].addEventListener('mouseenter', this.enterOptions);
       this.$refs['img-col'].addEventListener('mouseleave', this.leaveOptions);
     }
   },
   beforeDestroy() {
-    if (!this.smallScreen) {
+    if (!this.$vuetify.breakpoint.smAndDown) {
       this.$refs['img-col'].removeEventListener('mouseenter', this.enterOptions);
       this.$refs['img-col'].removeEventListener('mouseleave', this.leaveOptions);
     }
