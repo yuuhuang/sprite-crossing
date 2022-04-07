535ddabd515180a862d7700ed0be84b6af391d33
Neil <948459029@qq.com>
2022-04-07 13:50:06 +0800

    chore: restruct

--- serve/routers/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userControllers = require('../controllers/user');
-
-const router = express.Router();
-
-
-router.get('/getUserById', userControllers.getUserById);
-
-module.exports = router;
