7fe231dc191904a58e3cd837a850358027fde23e
Neil <948459029@qq.com>
2022-04-07 13:47:15 +0800

    feat: save serve

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 const express = require('express')
-const cors = require('cors');
+// const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
 
 
-app.use(cors());
+// Connect to MongoDB
+
+// eslint-disable-next-line max-len
+const dbUrl = 'mongodb+srv://yuuhuang:x4tDcuvCQ2rNSmK@sprite-crossing.8roz9.mongodb.net/sprite-crossing?retryWrites=true&w=majority';
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('connect to mongodb');
+    app.listen(3001, () => {
+      console.log(`http://localhost:3001/home`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+// Middlewares
+
 app.use(morgan('dev'));
+// app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use('/home', express.static(__dirname + '/ui/dist'));
-
-app.listen(3001, () => {
-  console.log(`listening at http://localhost:3001/home`);
-});
